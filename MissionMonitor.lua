028624ae53d3f5453ec473dabd560ce4e2e38b0d
Jan Van Buggenhout <chipzz@chipzz.be>
2024-07-28 00:34:22 +0200

    Better Options UI, TWW fixes.
    Too many changes to list, improved the options UI.
    
    A completely new options UI is in the works for 0.3.

--- MissionMonitor.lua
+++ MissionMonitor.lua
@@ -71,7 +71,7 @@ local function MissionMonitor_CheckMission(mission, alert_system, message)
 		if link then
 			mission_item_links[item] = link
 		else
-			item_info_add_callback(item, function(item, _, link)
+			item_info_add_callback(item, function(_, item, _, link)
 				if not mission_item_links then return end
 				mission_item_links[item] = link
 				print_mission_rewards()
@@ -85,12 +85,12 @@ local function MissionMonitor_CheckCompleteMission(mission)
 	MissionMonitor_CheckMission(
 		mission,
 		mission.followerTypeID == Enum.GarrisonFollowerType.FollowerType_6_0_Boat and GarrisonShipMissionAlertSystem or GarrisonMissionAlertSystem,
-		"%s is complete with wanted reward %s"
+		"%s is |c0000ff00complete|r with wanted reward %s"
 	)
 end
 
 local function MissionMonitor_CheckAvailableMission(mission)
-	MissionMonitor_CheckMission(mission, GarrisonRandomMissionAlertSystem, "%s is available with wanted reward %s")
+	MissionMonitor_CheckMission(mission, GarrisonRandomMissionAlertSystem, "%s is |c00ffff00available|r with wanted reward %s")
 end
 
 local follower_types = {}
@@ -144,10 +144,10 @@ event_frame:SetScript("OnEvent", function(self, event, ...)
 	elseif (event == "GARRISON_RANDOM_MISSION_ADDED") then
 		MissionMonitor_CheckAvailableMission(C_Garrison.GetBasicMissionInfo(select(2, ...)))
 	elseif (event == "GET_ITEM_INFO_RECEIVED") then
-		local item = ...
+		local item, success = ...
 		if not item_info_callbacks[item] then return end
 		for _, callback in ipairs(item_info_callbacks[item]) do
-			callback(item, GetItemInfo(item))
+			callback(success, item, GetItemInfo(item))
 		end
 		item_info_callbacks[item] = nil
 		if not next(item_info_callbacks, nil) then
@@ -159,11 +159,44 @@ end)
 MissionMonitorOptionsMixin = {}
 
 function MissionMonitorOptionsMixin:OnLoad()
-	InterfaceOptions_AddCategory(self)
+	Settings.RegisterAddOnCategory(Settings.RegisterCanvasLayoutCategory(self, self.name))
 	MissionMonitorListMixin = nil
 	MissionMonitorOptionsMixin = nil
 	self.OnLoad = nil
 end
+
+local function MissionMonitorListLine_SetText(self, text)
+	self.text:SetText(text)
+	self:SetHeight(self.text:GetStringHeight() + 3)
+end
+
+local function MissionMonitorListLine_SetEntry(self, entry)
+	local function set_item_link(link)
+		MissionMonitorListLine_SetText(self, LinkAddIcon(link, select(5, GetItemInfoInstant(entry)), 12))
+		self:Show()
+	end
+	local link = select(2, GetItemInfo(entry))
+	if link then
+		set_item_link(link)
+	else
+		item_info_add_callback(entry, function(_, _, _, link)
+			set_item_link(link)
+		end)
+	end
+end
+
+local function MissionMonitorListLine_Anchor(self)
+	self:SetPoint("LEFT")
+	self:SetPoint("RIGHT")
+	local parent = self:GetParent()
+	if self:GetID() == 1 then
+		self:SetPoint("TOP", parent.add, "BOTTOM", 0, -4)
+	else
+		self:SetPoint("TOP", parent.lines[self:GetID() - 1], "BOTTOM")
+	end
+end
+
+local line_pool = CreateFramePool("FRAME", nil, "MissionMonitorListLine")
 
 local function MissionMonitorList_GetVariable(self)
 	if not self.variableAffix then return end
@@ -171,39 +204,43 @@ local function MissionMonitorList_GetVariable(self)
 	return _G["MissionMonitor" .. self.variableAffix]
 end
 
+MissionMonitorListLineMixin = {}
+
+function MissionMonitorListLineMixin:Delete()
+	local i = self:GetID()
+	local list = self:GetParent()
+	local table = MissionMonitorList_GetVariable(list)
+	if not table or type(table) ~= "table" or i > #table then return end
+	tremove(table, i)
+	MissionMonitor_FlushWanted()
+
+	line_pool:Release(self)
+	tremove(list.lines, i)
+	for l = i, #list.lines do
+		list.lines[l]:SetID(l)
+	end
+	local line = list.lines[i]
+	if line then
+		MissionMonitorListLine_Anchor(line)
+	end
+end
+
+local function MissionMonitorList_AppendLine(self)
+	local i = #self.lines + 1
+	local line = line_pool:Acquire()
+	self.lines[i] = line
+	line:SetParent(self)
+	line:SetID(i)
+	MissionMonitorListLine_Anchor(line)
+	return line
+end
+
 local function MissionMonitorList_UpdateItems(self)
 	local entries = MissionMonitorList_GetVariable(self)
 	if not entries or type(entries) ~= "table" then return end
-	local entry_links = {}
-	local function set_lines()
-		local links = {}
-		local deletes = {}
-		for e, entry in pairs(entries) do
-			local link = entry_links[entry]
-			if link then
-				links[#links + 1] = LinkAddIcon(link, select(5, GetItemInfoInstant(entry)), 12)
-				deletes[#links] = format("|Hd:%i|h[Delete]|h", e)
-			else
-				return
-			end
-		end
-		entry_links = nil
-		self.lines:SetText(strjoin("|n", unpack(links)))
-		self.deletes:SetText(strjoin("|n", unpack(deletes)))
-		self:SetHeight(self.title:GetStringHeight() + 8 + self.add:GetHeight() + 4 + self.lines:GetStringHeight())
-	end
 	for i, entry in ipairs(entries) do
-		local link = select(2, GetItemInfo(entry))
-		if link then
-			entry_links[entry] = link
-		else
-			item_info_add_callback(entry, function(entry, _, link)
-				entry_links[entry] = link
-				set_lines()
-			end)
-		end
+		MissionMonitorListLine_SetEntry(self.lines[i] or MissionMonitorList_AppendLine(self), entry)
 	end
-	set_lines()
 end
 
 local function MissionMonitorList_Add(self, text)
@@ -217,23 +254,8 @@ local function MissionMonitorList_Add(self, text)
 	wanted_items = { [number] = 1 }
 	MissionMonitor_RecheckMissions()
 	MissionMonitor_FlushWanted()
-	MissionMonitorList_UpdateItems(self)
+	MissionMonitorListLine_SetEntry(MissionMonitorList_AppendLine(self), number)
 	self.input:SetText("")
-end
-
-local function MissionMonitorList_OnHyperlinkClick(self, link, text, button)
-	local t, i = strsplit(":", link)
-	if t == 'd' then
-		local number = tonumber(i)
-		if number == nil then return end
-		local table = MissionMonitorList_GetVariable(self)
-		if not table or type(table) ~= "table" or number > #table then return end
-		tremove(table, number)
-		MissionMonitor_FlushWanted()
-		MissionMonitorList_UpdateItems(self)
-	else
-		SetItemRef(link, text, button)
-	end
 end
 
 MissionMonitorListMixin = {}
@@ -246,5 +268,5 @@ function MissionMonitorListMixin:OnLoad()
 	self.add:SetScript("OnClick", function(self) MissionMonitorList_Add(self:GetParent(), self:GetParent().input:GetText()) end)
 	self.input:SetScript("OnEnterPressed", function(self) MissionMonitorList_Add(self:GetParent(), self:GetText()) end)
 	self:SetScript("OnShow", MissionMonitorList_UpdateItems)
-	self:SetScript("OnHyperlinkClick", MissionMonitorList_OnHyperlinkClick)
+	self.lines = {}
 end
